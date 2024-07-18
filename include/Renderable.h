3ece4f7d487fb828007f4e825ce1362d9eca2c1a
Thunder930 <97140451+Thunder930@users.noreply.github.com>
2024-07-18 19:22:45 -0400

    Initalize renderables array pointer to null

--- include/Renderable.h
+++ include/Renderable.h
@@ -6,6 +6,6 @@ public:
 	virtual void Render() const = 0;
 	virtual void Update(double deltaTime) = 0;
 protected:
-	std::vector<Renderable*> *renderables;
+	std::vector<Renderable*> *renderables = nullptr;
 };
 
